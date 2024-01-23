089b9a43032010dd72ff808a1f1b337cbdab05cc
SpiderFB <arnab9954@gmail.com>
2024-01-23 18:50:45 +0530

    supper and normal user both

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, HttpResponse
+import uuid
+from .models import NewUser
 
 # Create your views here.
 def home(request):
-    
     if request.method == "POST":
         email = request.POST['email']
         password = request.POST['password']
         phone = request.POST['phone']
+        user_uuid = uuid.uuid4()  # Generate a new UUID
 
-        print(email, password, phone)
+        print(email, password, phone, user_uuid)
+        user = NewUser(email=email, phone=phone, is_staff=False, is_superuser=False)
+        user.set_password('password')
+        user.save()
 
         return HttpResponse("Okay")
     else:
         return render(request, 'accounts/accounts.html')
-

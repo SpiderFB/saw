4150c3e49a93b845223d777b8f28beee333a7f4b
SpiderFB <arnab9954@gmail.com>
2024-01-24 14:59:21 +0530

    SignUP & SignIn

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='')
+    path('', views.accounts_home, name=''),
+    path('signup/', views.user_signup, name=''),
+    path('signin/', views.user_signin, name=''),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, HttpResponse
 import uuid
 from .models import NewUser
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-def home(request):
+def accounts_home(request):
+    return render(request, 'accounts/accounts.html')
+
+def user_signup(request):
     if request.method == "POST":
         email = request.POST['email']
         password = request.POST['password']
@@ -12,9 +16,26 @@ def home(request):
 
         print(email, password, phone, user_uuid)
         user = NewUser(email=email, phone=phone, is_staff=False, is_superuser=False)
-        user.set_password('password')
+        user.set_password(password)
         user.save()
 
         return HttpResponse("Okay")
     else:
-        return render(request, 'accounts/accounts.html')
+        return render(request, 'accounts/signup.html')
+    
+    
+def user_signin(request):
+
+    if request.method == "POST":
+        email = request.POST['email123']
+        password = request.POST['password123']
+        # print(username, password)
+        user = authenticate(request, email=email, password=password)
+        print (user)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Got it")
+        else:
+            return render(request, 'accounts/signin.html')
+    else:
+        return render(request, 'accounts/signin.html')

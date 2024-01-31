97b0a1731825bd2fcee2afd3c086e68b10b73389
SpiderFB <arnab9954@gmail.com>
2024-01-31 09:29:24 +0530

    Delete working properly

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,7 @@ from .import views
 urlpatterns = [
     path('cart/', views.cart_home, name='cart'),
     path('addtocart/<uuid:product_uuid>/', views.addtocart, name=''),
-    path('removefromcart/<uuid:product_uuid>/', views.removefromcart, name=''),
+    path('reducefromcart/<uuid:product_uuid>/', views.reducefromcart, name=''),
+    path('deletefromcart/<uuid:product_uuid>/', views.deletefromcart, name=''),
     
 ]
--- cart/views.py
+++ cart/views.py
@@ -40,13 +40,22 @@ def addtocart(request, product_uuid):
     
     return redirect('cart')
 
-def removefromcart(request, product_uuid):
+def reducefromcart(request, product_uuid):
 
     cart= Cart.objects.get(user_uuid=request.user)
     product = get_object_or_404(productdb, product_uuid=product_uuid)
     cart_item= CartItem.objects.get(cart=cart, product=product)
     cart_item.quantity -= 1
     cart_item.save()
+    if cart_item.quantity == 0:
+            cart_item.delete()
+    cart.save()   
+    return redirect('cart')
+
+def deletefromcart(request, product_uuid):
+    cart= Cart.objects.get(user_uuid=request.user)
+    product = get_object_or_404(productdb, product_uuid=product_uuid)
+    cart_item= CartItem.objects.get(cart=cart, product=product)
+    cart_item.delete()
     cart.save()
-    
     return redirect('cart')
